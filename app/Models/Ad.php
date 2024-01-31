f365dd8393026d6415e772561dd881d89e1757dc
BadreddineZatout <hb_zatout@esi.dz>
2024-01-31 21:56:28 +0100

    feat: add models

---
+++ app/Models/Ad.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Ad extends Model implements HasMedia
+{
+    use InteractsWithMedia;
+
+    protected $fillable = ['name', 'description', 'category_id', 'location_id', 'price', 'vues', 'is_premium',  'is_published', 'published_at'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
+}
