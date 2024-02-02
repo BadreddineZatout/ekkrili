92b8066221f71c760a5600ab3791cb35888daa70
BadreddineZatout <hb_zatout@esi.dz>
2024-02-02 16:19:49 +0100

    feat: add location creation process

--- app/Filament/Resources/AdResource/Pages/CreateAd.php
+++ app/Filament/Resources/AdResource/Pages/CreateAd.php
@@ -2,10 +2,29 @@
 
 namespace App\Filament\Resources\AdResource\Pages;
 
+use App\Models\Location;
 use App\Filament\Resources\AdResource;
+use Illuminate\Database\Eloquent\Model;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateAd extends CreateRecord
 {
     protected static string $resource = AdResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $location = Location::create([
+            'address' => $this->data['address'],
+            'postal_code' => $this->data['postal_code'],
+            'city' => $this->data['city'],
+            'state' => $this->data['state'],
+            'latitude' => $this->data['latitude'],
+            'longitude' => $this->data['longitude'],
+        ]);
+
+        return static::getModel()::create([
+            ...$data,
+            'location_id' => $location->id
+        ]);
+    }
 }
