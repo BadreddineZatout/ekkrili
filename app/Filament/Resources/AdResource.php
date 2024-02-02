a21a70b8f90c06d03c6418aeb47d911aaf522c62
BadreddineZatout <hb_zatout@esi.dz>
2024-02-02 16:09:06 +0100

    feat: add ad resource

---
+++ app/Filament/Resources/AdResource.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AdResource\Pages;
+use App\Models\Ad;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class AdResource extends Resource
+{
+    protected static ?string $model = Ad::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-home-modern';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Select::make('type')
+                    ->options([
+                        0 => 'Louer',
+                        1 => 'Acheter',
+                    ])
+                    ->required(),
+                Forms\Components\Select::make('category_id')
+                    ->relationship('category', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->maxLength(65535)
+                    ->columnSpanFull(),
+                Section::make('Location')
+                    ->description('Add the location of the property')
+                    ->columns(2)
+                    ->schema([
+                        Forms\Components\TextInput::make('address')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('city')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('state')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('postal_code')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('latitude')
+                            ->required()
+                            ->numeric(),
+                        Forms\Components\TextInput::make('longitude')
+                            ->required()
+                            ->numeric(),
+                    ]),
+                Forms\Components\Toggle::make('is_published')
+                    ->live()
+                    ->required(),
+                Forms\Components\Toggle::make('is_premium')
+                    ->required(),
+                Forms\Components\DatePicker::make('published_at')
+                    ->visible(fn (Get $get) => $get('is_published')),
+                Forms\Components\TextInput::make('vues')
+                    ->required()
+                    ->numeric()
+                    ->hiddenOn('create'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->money()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('category.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('location.address')
+                    ->limit(50)
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('is_premium')
+                    ->boolean(),
+                Tables\Columns\IconColumn::make('is_published')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('published_at')
+                    ->date()
+                    ->default('---')
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('vues')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAds::route('/'),
+            'create' => Pages\CreateAd::route('/create'),
+            'view' => Pages\ViewAd::route('/{record}'),
+            'edit' => Pages\EditAd::route('/{record}/edit'),
+        ];
+    }
+}
