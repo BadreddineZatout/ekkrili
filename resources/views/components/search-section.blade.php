69d1262c532a4644048480fa9211b77823c5ad24
BadreddineZatout <hb_zatout@esi.dz>
2024-02-02 23:02:58 +0100

    style: apply gold color

--- resources/views/components/search-section.blade.php
+++ resources/views/components/search-section.blade.php
@@ -5,33 +5,81 @@
     <div class="w-full flex justify-center">
         <div class="w-1/3 shadow-md rounded-md">
             <div class="flex items-center">
-                <button id="louer" class="w-1/2 rounded-t-md bg-blue-500 text-white py-2">Louer</button>
-                <button id="acheter" class="w-1/2 rounded-t-md bg-blue-500 text-white py-2">Acheter</button>
+                <button id="louer"
+                    class="w-1/2 rounded-t-md border-2 border-gold-200 bg-gold-500 text-white py-2">Louer</button>
+                <button id="acheter"
+                    class="w-1/2 rounded-t-md border-2 border-gold-200 bg-gold-500 text-white py-2">Acheter</button>
             </div>
-            <form class="px-5 py-10 w-full">
+            <form class="px-5 py-10 w-full border border-t-0 border-gold-400 rounded-b-md">
                 <div class="w-full mb-5">
-                    <input class=" w-full px-3 py-2 border border-gray-300 rounded-md" type="text"
-                        placeholder="Entrez un mot clé ...">
+                    <input
+                        class=" w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gold-400"
+                        type="text" placeholder="Entrez un mot clé ...">
                 </div>
                 <div class="w-full flex gap-x-3">
-                    <input class="w-3/5 px-3 py-2 border border-gray-300 rounded-md" type="text"
-                        placeholder="Dans quelle ville ? Quartier ?">
+                    <input
+                        class="w-3/5 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gold-400"
+                        type="text" placeholder="Dans quelle ville ? Quartier ?">
 
-                    <input class="w-2/5 px-3 py-2 border border-gray-300 rounded-md" type="text"
-                        placeholder="Votre budget max ?">
+                    <input
+                        class="w-2/5 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gold-400"
+                        type="text" placeholder="Votre budget max ?">
                 </div>
-                <div class="mt-5">
-                    <span id="radio-1" class="px-3 py-2 border border-dashed border-gray-400 rounded-md"><input
-                            type="radio" name="type" id="maison"> <label for="maison">Maison</label></span>
-                    <span id="radio-2" class="px-3 py-2 border border-dashed border-gray-400 rounded-md"><input
-                            type="radio" name="type" id="appartement"> <label
-                            for="appartement">Appartement</label></span>
-                    <span id="radio-3" class="px-3 py-2 border border-dashed border-gray-400 rounded-md"><input
-                            type="radio" name="type" id="evenement"> <label
-                            for="evenement">Evenement</label></span>
+                <div class="mt-5 flex flex-wrap gap-3">
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="type" />
+                        <div
+                            class="w-fit max-w-xl rounded-md p-2 text-gray-300 ring-2 ring-gold-200 transition-all hover:shadow peer-checked:text-gold-600 peer-checked:ring-gold-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between gap-x-3">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">Maison</p>
+                                    <div>
+                                        <svg width="24" height="24" viewBox="0 0 24 24">
+                                            <path fill="currentColor"
+                                                d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="type" />
+                        <div
+                            class="w-fit max-w-xl rounded-md bg-white p-2 text-gray-300 ring-2 ring-gold-200 transition-all hover:shadow peer-checked:text-gold-600 peer-checked:ring-gold-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between gap-x-3">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">Appartement</p>
+                                    <div>
+                                        <svg width="24" height="24" viewBox="0 0 24 24">
+                                            <path fill="currentColor"
+                                                d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
+                    <label class="cursor-pointer">
+                        <input type="radio" class="peer sr-only" name="type" />
+                        <div
+                            class="w-fit max-w-xl rounded-md bg-white p-2 text-gray-300 ring-2 ring-gold-200 transition-all hover:shadow peer-checked:text-gold-600 peer-checked:ring-gold-400 peer-checked:ring-offset-2">
+                            <div class="flex flex-col gap-1">
+                                <div class="flex items-center justify-between gap-x-3">
+                                    <p class="text-sm font-semibold uppercase text-gray-500">Evenement</p>
+                                    <div>
+                                        <svg width="24" height="24" viewBox="0 0 24 24">
+                                            <path fill="currentColor"
+                                                d="m10.6 13.8l-2.175-2.175q-.275-.275-.675-.275t-.7.3q-.275.275-.275.7q0 .425.275.7L9.9 15.9q.275.275.7.275q.425 0 .7-.275l5.675-5.675q.275-.275.275-.675t-.3-.7q-.275-.275-.7-.275q-.425 0-.7.275ZM12 22q-2.075 0-3.9-.788q-1.825-.787-3.175-2.137q-1.35-1.35-2.137-3.175Q2 14.075 2 12t.788-3.9q.787-1.825 2.137-3.175q1.35-1.35 3.175-2.138Q9.925 2 12 2t3.9.787q1.825.788 3.175 2.138q1.35 1.35 2.137 3.175Q22 9.925 22 12t-.788 3.9q-.787 1.825-2.137 3.175q-1.35 1.35-3.175 2.137Q14.075 22 12 22Z" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </label>
                 </div>
                 <div class="w-full text-center mt-10">
-                    <button class="px-5 py-3 rounded-full bg-blue-600 text-white hover:bg-blue-500">
+                    <button class="px-5 py-3 rounded-full bg-gold-500 text-white hover:bg-gold-300">
                         Rechercher
                     </button>
                 </div>
@@ -41,46 +89,22 @@
 </div>
 <script>
     $(document).ready(function() {
-        $("#louer").removeClass("bg-blue-500 text-white");
-        $("#louer").addClass("bg-white text-black");
+        $("#louer").addClass("bg-gold-500 text-white");
+        $("#acheter").addClass("bg-white text-black");
+        $("#acheter").removeClass("bg-gold-500 text-white");
     });
     $("#louer").click(function(e) {
         e.preventDefault();
-        $("#louer").removeClass("bg-blue-500 text-white");
-        $("#louer").addClass("bg-white text-black");
-        $("#acheter").removeClass("bg-white text-black");
-        $("#acheter").addClass("bg-blue-500 text-white");
+        $("#louer").addClass("bg-gold-500 text-white");
+        $("#louer").removeClass("bg-white text-black");
+        $("#acheter").addClass("bg-white text-black");
+        $("#acheter").removeClass("bg-gold-500 text-white");
     });
     $("#acheter").click(function(e) {
         e.preventDefault();
-        $("#acheter").removeClass("bg-blue-500 text-white");
-        $("#acheter").addClass("bg-white text-black");
-        $("#louer").removeClass("bg-white text-black");
-        $("#louer").addClass("bg-blue-500 text-white");
-    });
-
-    $("#radio-1").click(function(e) {
-        $("#radio-1").removeClass("border-dashed border-gray-400");
-        $("#radio-1").addClass("border-solid border-blue-400");
-        $("#radio-2").removeClass("border-solid border-blue-400");
-        $("#radio-3").removeClass("border-solid border-blue-400");
-        $("#radio-2").addClass("border-dashed border-gray-400");
-        $("#radio-3").addClass("border-dashed border-gray-400");
-    });
-    $("#radio-2").click(function(e) {
-        $("#radio-2").removeClass("border-dashed border-gray-400");
-        $("#radio-2").addClass("border-solid border-blue-400");
-        $("#radio-1").removeClass("border-solid border-blue-400");
-        $("#radio-3").removeClass("border-solid border-blue-400");
-        $("#radio-1").addClass("border-dashed border-gray-400");
-        $("#radio-3").addClass("border-dashed border-gray-400");
-    });
-    $("#radio-3").click(function(e) {
-        $("#radio-3").removeClass("border-dashed border-gray-400");
-        $("#radio-3").addClass("border-solid border-blue-400");
-        $("#radio-1").removeClass("border-solid border-blue-400");
-        $("#radio-2").removeClass("border-solid border-blue-400");
-        $("#radio-1").addClass("border-dashed border-gray-400");
-        $("#radio-2").addClass("border-dashed border-gray-400");
+        $("#acheter").addClass("bg-gold-500 text-white");
+        $("#acheter").removeClass("bg-white text-black");
+        $("#louer").addClass("bg-white text-black");
+        $("#louer").removeClass("bg-gold-500 text-white");
     });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,23 @@ export default {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gold: {
+                    50: "#fefce8",
+                    100: "#fffac2",
+                    200: "#fff289",
+                    300: "#ffe345",
+                    400: "#fdd017",
+                    500: "#edb505",
+                    600: "#cd8c01",
+                    700: "#a36305",
+                    800: "#874e0c",
+                    900: "#724011",
+                    950: "#432005",
+                },
+            },
+        },
     },
     plugins: [],
 };
