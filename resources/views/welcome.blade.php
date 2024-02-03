f1da8f6bc3812eec7c79bf872bb67b92e95bc6ba
BadreddineZatout <hb_zatout@esi.dz>
2024-02-03 23:25:27 +0100

    feat: last renting ads section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,5 @@
     <x-search-section />
     <x-premium-ads :ads="$premium_ads" />
     <x-last-sales-ads :ads="$last_sales" />
-    <x-last-renting-ads />
+    <x-last-renting-ads :ads="$last_rentals" />
 @endsection
