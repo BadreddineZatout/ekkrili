b46d48f8ba9ee3db2b184664d948b7ee613234ad
BadreddineZatout <hb_zatout@esi.dz>
2024-02-04 22:36:34 +0100

    feat: implement ad search function

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -12,6 +12,27 @@ class AdController extends Controller
         $limit = $request->query('limit', 20);
         $offset = $request->query('offset', 0);
         $ads_query = Ad::latest();
+
+        if ($search = $request->query('search')) {
+            $ads_query->where('name', 'like', "%$search%");
+        }
+
+        if ($type = $request->query('type')) {
+            $ads_query->where('type', $type);
+        }
+
+        if ($location = $request->query('location')) {
+            $ads_query->whereHas('location', function ($query) use ($location) {
+                return $query->where('address', 'like', "%$location%")
+                    ->orWhere('city', 'like', "%$location%")
+                    ->orWhere('state', 'like', "%$location%");
+            });
+        }
+
+        if ($price = $request->query('price')) {
+            $ads_query->where('price', '<=', $price);
+        }
+
         $ads = $ads_query->offset($offset * $limit)->limit($limit)->get();
 
         return view('ads', compact('ads'));
