7fa951aa6fa99473baeba631ad088a961a21ad53
BadreddineZatout <hb_zatout@esi.dz>
2024-02-04 20:35:55 +0100

    feat: ads page

--- app/Http/Controllers/AdController.php
+++ app/Http/Controllers/AdController.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ad;
+use Illuminate\Http\Request;
+
 class AdController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $limit = $request->query('limit', 20);
+        $offset = $request->query('offset', 0);
+        $ads_query = Ad::latest();
+        $ads = $ads_query->offset($offset * $limit)->limit($limit)->get();
+
+        return view('ads', compact('ads'));
+    }
 }
---
+++ resources/views/ads.blade.php
@@ -0,0 +1,17 @@
+@extends('layout')
+
+@section('content')
+    <div class="w-full flex gap-x-5 px-20 mt-10">
+        <div class="w-1/4 border border-gold-100">
+            <x-filters />
+        </div>
+        <div class="w-3/4">
+            <h1 class="text-2xl font-semibold">{{ count($ads) }} résultats trouvés</h1>
+            <div class="w-full mt-10 px-5 flex flex-wrap gap-y-5">
+                @foreach ($ads as $ad)
+                    <x-ad-card :ad="$ad" />
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
