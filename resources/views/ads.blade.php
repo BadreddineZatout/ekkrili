99ca21f39204639416de0413641b994bb413a290
BadreddineZatout <hb_zatout@esi.dz>
2024-02-09 15:18:42 +0100

    style: responsive design for ads page

--- resources/views/ads.blade.php
+++ resources/views/ads.blade.php
@@ -1,11 +1,11 @@
 @extends('layout')
 
 @section('content')
-    <div class="w-full flex gap-x-5 px-20 mt-10 mb-5">
-        <div class="w-1/4 h-fit border border-gold-100">
+    <div class="w-full flex flex-wrap sm:flex-nowrap gap-5 px-5 sm:px-20 mt-10 mb-5">
+        <div class="w-full sm:w-1/4 h-fit border border-gold-100">
             <x-filters :categories="$categories" :filters="$filters" />
         </div>
-        <div class="w-3/4">
+        <div class="w-full sm:w-3/4">
             <h1 class="text-2xl font-semibold">{{ count($ads) }} résultats trouvés</h1>
             <div class="w-full my-10 px-5 flex flex-wrap gap-y-5">
                 @foreach ($ads as $ad)
